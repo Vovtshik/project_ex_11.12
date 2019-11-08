edb7416595a4787264bbb97d56c65c38b4828d58
Volodymir Konovalov <vvkon@meta.ua>
2019-11-08 09:18:57 +0200

    Update SourceE1112.cpp

--- SourceE1112.cpp
+++ SourceE1112.cpp
@@ -7,6 +7,25 @@ void out_file_text(vector<string>& vs, string& name_file);
 
 int main()
 {
-
+    vector<string>vs;
+    cout << "Enter file name:\n";
+    string name;
+    cin >> name;
+    in_file_text(name, vs);
+    for(string str: vs)
+    { 
+        cout << str << '\n';
+    }
     return 0;
+}
+
+void in_file_text(string& name_file, vector<string>&vs)
+{
+    ifstream ist(name_file);
+    if(!ist) error("Unable to open input file ", name_file);
+    ist.exceptions(ist.exceptions() | ios_base::badbit);
+    for(string temp; ist >> temp;)
+    {
+        vs.push_back(temp);
+    }
 }
