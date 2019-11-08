a270759f5fb68deb6fca12700cce6241f9eada8e
Volodymir Konovalov <vvkon@meta.ua>
2019-11-08 10:44:14 +0200

    Update SourceE1112.cpp

--- SourceE1112.cpp
+++ SourceE1112.cpp
@@ -2,7 +2,7 @@
 
 void in_file_text(string& name_file, vector<string>&vs);
 void reverse_word_order(vector<string>& vs1, vector<string>& vs2);
-string Change_order_characters(string& str);
+string change_order_characters(string& str);
 void out_file_text(vector<string>& vs, string& name_file);
 
 int main()
