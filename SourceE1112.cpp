bac37eb70bb3abfc8b3645ec3b12274613cc3a43
Volodymir Konovalov <vvkon@meta.ua>
2019-11-08 09:03:34 +0200

    Update SourceE1112.cpp

--- SourceE1112.cpp
+++ SourceE1112.cpp
@@ -1,5 +1,10 @@
 #include "../std_lib_facilities.h"
 
+void in_file_text(string& name_file, vector<string>&vs);
+void reverse_word_order(vector<string>& vs1, vector<string>& vs2);
+string Change_order_characters(string& str);
+void out_file_text(vector<string>& vs, string& name_file);
+
 int main()
 {
 
