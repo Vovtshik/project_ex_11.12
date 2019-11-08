34623b77f6b83fde1c4fccb736770b136a265e7c
Volodymir Konovalov <vvkon@meta.ua>
2019-11-08 12:10:26 +0200

    Update SourceE1112.cpp

--- SourceE1112.cpp
+++ SourceE1112.cpp
@@ -40,9 +40,7 @@ void in_file_text(string& name_file, vector<string>&vs)
                 vs.push_back(s);
             }
         }
-        
-        if(!ist.eof())
-            vs.push_back(new_line);
+        vs.push_back(new_line);
     }
 }
 
